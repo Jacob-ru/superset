8912949f0a9d6ce4a3138bcfd0fd6f3bd6d796b8
Яков Михопаркин <ja.mix@mail.ru>
2023-08-27 17:51:10 +0300

    новый макрос в хендлбарс

--- superset-frontend/plugins/plugin-chart-handlebars/src/components/Handlebars/HandlebarsViewer.tsx
+++ superset-frontend/plugins/plugin-chart-handlebars/src/components/Handlebars/HandlebarsViewer.tsx
@@ -99,8 +99,8 @@ Handlebars.registerHelper(
 );
 
 Handlebars.registerHelper('toShortened', (number: any) => {
-  if (number >= 1000000) return `${(number / 1000000).toFixed(2)} млн.`;
-  if (number >= 1000) return `${(number / 1000).toFixed(2)} тыс.`;
+  if (Math.abs(number) >= 1000000) return `${(number / 1000000).toFixed(2)} млн.`;
+  if (Math.abs(number) >= 1000) return `${(number / 1000).toFixed(2)} тыс.`;
   return `${number.toFixed(2)}`;
 });
 
